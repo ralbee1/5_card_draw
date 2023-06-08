988341298591a4a4ecebbb6c804d649aa07efebf
ralbee1 <93877353+ralbee1@users.noreply.github.com>
2023-06-07 23:48:54 -0500

    Pylint fixes and updated background image name.

--- video_poker/PAGEGUI.py
+++ video_poker/PAGEGUI.py
@@ -19,7 +19,7 @@ asset_file_directory = os.path.join(local_file_directory + '\Assets')
 
 def vp_start_gui():
     '''Starting point when module is the main routine.'''
-    global val, w, root
+    global root
     global prog_location
     prog_call = sys.argv[0]
     prog_location = os.path.split(prog_call)[0]
@@ -60,10 +60,10 @@ class Credits:
         default_card_back_file = tk.PhotoImage(file=default_card_back_file)
 
         #Initialize the bottom center display the number of winnings
-        self.Winnings = tk.Message(top)
-        self.Winnings.place(relx=0.026, rely=0.877, relheight=0.093, relwidth=0.178)
-        self.Winnings.config(font=("Courier", 44))
-        self.Winnings.configure(
+        self.player_winnings_display = tk.Message(top)
+        self.player_winnings_display.place(relx=0.026, rely=0.877, relheight=0.093, relwidth=0.178)
+        self.player_winnings_display.config(font=("Courier", 44))
+        self.player_winnings_display.configure(
             background="#00008b",
             foreground="#cc3300",
             highlightbackground="#d9d9d9",
@@ -87,7 +87,7 @@ class Credits:
         #Initialize the banner image at the top
         self.banner = tk.Button(top)
         self.banner.place(relx=0.015, rely=0.019, height=300, width=1800)
-        banner_image = (os.path.join(asset_file_directory + '\BackgroundImage3.png'))
+        banner_image = os.path.join(asset_file_directory + '\BackgroundImage.png')
         banner_image = tk.PhotoImage(file=banner_image)
         self.banner.configure(
             activebackground="#ececec",
@@ -164,10 +164,10 @@ class Credits:
         )
 
         #Intialize card 1 (Furthest card left)
-        self.Card1 = tk.Button(top)
-        self.Card1.place(relx=0.016, rely=0.386, height=508, width=350)
-        self.Card1.configure(activebackground="#ececec")
-        self.Card1.configure(
+        self.card_one = tk.Button(top)
+        self.card_one.place(relx=0.016, rely=0.386, height=508, width=350)
+        self.card_one.configure(activebackground="#ececec")
+        self.card_one.configure(
             activeforeground="#000000",
             background="#d9d9d9",
             disabledforeground="#a3a3a3",
@@ -179,12 +179,12 @@ class Credits:
             command= lambda: self.toggle_card_hold(1),
             text='''Button'''
         )
-        self.Card1.Image = default_card_back_file
+        self.card_one.Image = default_card_back_file
 
         #Intialize card 2
-        self.Card2 = tk.Button(top)
-        self.Card2.place(relx=0.203, rely=0.386, height=508, width=350)
-        self.Card2.configure(
+        self.card_two = tk.Button(top)
+        self.card_two.place(relx=0.203, rely=0.386, height=508, width=350)
+        self.card_two.configure(
             activebackground="#ececec",
             activeforeground="#000000",
             background="#d9d9d9",
@@ -197,15 +197,15 @@ class Credits:
             command=lambda: self.toggle_card_hold(2),
             text='''Button'''
         )
-        self.Card2.Image = default_card_back_file
+        self.card_two.Image = default_card_back_file
 
         self.menubar = tk.Menu(top,font="TkMenuFont",bg=_bgcolor,fg=_fgcolor)
         top.configure(menu = self.menubar)
 
         #Intialize card 3
-        self.Card3 = tk.Button(top)
-        self.Card3.place(relx=0.391, rely=0.386, height=508, width=350)
-        self.Card3.configure(
+        self.card_three = tk.Button(top)
+        self.card_three.place(relx=0.391, rely=0.386, height=508, width=350)
+        self.card_three.configure(
             activebackground="#ececec",
             activeforeground="#000000",
             background="#d9d9d9",
@@ -218,12 +218,12 @@ class Credits:
             command=lambda: self.toggle_card_hold(3),
             text='''Button'''
         )
-        self.Card3.Image = default_card_back_file
+        self.card_three.Image = default_card_back_file
 
         #Intialize card 4
-        self.Card4 = tk.Button(top)
-        self.Card4.place(relx=0.578, rely=0.386, height=508, width=350)
-        self.Card4.configure(
+        self.card_four = tk.Button(top)
+        self.card_four.place(relx=0.578, rely=0.386, height=508, width=350)
+        self.card_four.configure(
             activebackground="#ececec",
             activeforeground="#000000",
             background="#d9d9d9",
@@ -236,12 +236,12 @@ class Credits:
             command=lambda: self.toggle_card_hold(4),
             text='''Button'''
         )
-        self.Card4.Image = default_card_back_file
+        self.card_four.Image = default_card_back_file
 
         #Intialize card 5
-        self.Card5 = tk.Button(top)
-        self.Card5.place(relx=0.766, rely=0.386, height=508, width=350)
-        self.Card5.configure(
+        self.card_five = tk.Button(top)
+        self.card_five.place(relx=0.766, rely=0.386, height=508, width=350)
+        self.card_five.configure(
             activebackground="#ececec",
             activeforeground="#000000",
             background="#d9d9d9",
@@ -254,13 +254,13 @@ class Credits:
             command=lambda: self.toggle_card_hold(5),
             text='''Button'''
         )
-        self.Card5.Image = default_card_back_file
+        self.card_five.Image = default_card_back_file
 
         #Initialize the bet_one button
-        self.Bet_One = tk.Button(top)
-        self.Bet_One.place(relx=0.214, rely=0.877, height=90, width=150)
-        self.Bet_One.config(font=("Courier", 22))
-        self.Bet_One.configure(
+        self.bet_one_button = tk.Button(top)
+        self.bet_one_button.place(relx=0.214, rely=0.877, height=90, width=150)
+        self.bet_one_button.config(font=("Courier", 22))
+        self.bet_one_button.configure(
             activebackground="#ececec",
             activeforeground="#000000",
             background="#d9d9d9",
@@ -342,14 +342,14 @@ class Credits:
         print(f' Player Hand: {PLAYERHAND}')
 
         #This list is used to update the card images for both redrawing and starting new hands.
-        card_list = [self.Card1, self.Card2, self.Card3, self.Card4, self.Card5]
-        
+        card_list = [self.card_one, self.card_two, self.card_three, self.card_four, self.card_five]
+
         if self.status_deal_button:
             print("Starting New Hand")
             #Creating player hand and clearing previous winning hand rank and winnings.
             PLAYERHAND, DECK = video_poker_functions.create_hand(DECK)
             self.winning_hand.configure(text='')
-            self.Winnings.configure(text='')
+            self.player_winnings_display.configure(text='')
 
             #Subract Bet Amount
             PLAYERMONEY = PLAYERMONEY - (self.bet_amount + 1)
@@ -389,7 +389,7 @@ class Credits:
 
             #Calculate and Display Winnings
             hand_winnings = video_poker_functions.calculate_payout(hand_score, self.bet_amount)
-            self.Winnings.configure(text=hand_winnings)
+            self.player_winnings_display.configure(text=hand_winnings)
             print(f'Player won ${hand_winnings}')
 
             #Provide playout to player balance and update player credit balance
@@ -427,10 +427,12 @@ class Credits:
     def bet_max_command(self):
         '''Sets the bet to the maximum. Bet amount is 4 since we start at 0.'''
         print("Bet Max Button")
+
         #Disallow changing bet mid hand by checking status_deal_button
         if self.status_deal_button:
             self.bet_amount = 4
             self.current_bet.configure(text='''5''')
+        root.update_idletasks()
 
 
     def bet_one_command(self):
@@ -444,6 +446,8 @@ class Credits:
             else:
                 self.bet_amount += 1
                 self.current_bet.configure(text=self.bet_amount + 1)
+        root.update_idletasks()
+
 
 if __name__ == '__main__':
     '''Starts the GUI and begin the program.'''
